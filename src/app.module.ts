30c1462ad7af48fc95a1f402093cc363b20e6207
Raj Gohil <raj.gohil@tatvasoft.com>
2023-05-04 19:02:02 +0530

    Database setup with typeORM with users and reports repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      host: 'localhost',
+      entities: [User, Report],
+      username: 'postgres',
+      password: 'root',
+      database: 'nestjs-my-car-value',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
