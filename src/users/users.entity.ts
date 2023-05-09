c82092bd95434c7c5b09288c87598193a81bce45
Raj Gohil <raj.gohil@tatvasoft.com>
2023-05-09 11:11:20 +0530

    Added the hooks.

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,22 @@ export class User {
 
   @Column()
   password: string;
+
+  // Have implemented the hooks below to log the create, Update and delete operations.
+  @AfterInsert()
+  logAfterCreate() {
+    console.log(
+      `Creating the record of user having the email: ${this.email} and id: ${this.id}`,
+    );
+  }
+
+  @AfterUpdate()
+  logAfterUpdate() {
+    console.log(`Updating the record for the id: ${this.id}`);
+  }
+
+  @AfterRemove()
+  logAfterRemove() {
+    console.log(`Deleted the user record of id: ${this.id}`);
+  }
 }
